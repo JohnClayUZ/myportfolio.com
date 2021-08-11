c0d671861ef468fa0eb46faff1afd9f5185b0a9b
Doniyor Majidov <majidov.doniyor007@gmail.com>
2021-08-11 16:03:52 +0500

    doniy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./global.scss";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+
+const figma={
+    umum:[
+        {
+            img:"images/Group1.png",
+            text:"Adobe Photoshop",
+            star:"images/star.png"
+        },
+        {
+            img:"images/Group 2.png",
+            text:"Adobe Illustrator",
+            star:"images/star.png"
+        },
+        {
+            img:"images/Group3.png",
+            text:"Adobe After Effects",
+            star:"images/star.png"
+        },
+        {
+            img:"images/Group4.png",
+            text:"Figma",
+            star:"images/star.png"
+        },
+    ]
+};
+
+function Skills(state=figma,action) {
+    return state
+}
+
+const store = createStore(combineReducers({Skills}));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
